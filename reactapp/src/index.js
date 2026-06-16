2a24eafda9a391b6034b5583afd9c8d1ec9384f8
ZikAsakur <khairutdinovraushan@gmail.com>
2026-06-17 01:46:15 +0300

    f7

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -20,12 +20,10 @@ export const API_MEDIA = "https://gilzea.pythonanywhere.com";
 //export const API_URL = "http://localhost:8000/api/hacaton/";  
 //export const API_MEDIA = "http://localhost:8000";
 
-
-const basename = '/hacaton';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <BrowserRouter>
         <Routes>
           <Route path="/" element={<App/>} />
           <Route path="/event" element={<Events />} />
